16dc37f3144caeb961195ff6aa7e2fafdfd842cf
Eduard Bess <eduard.bess@gmail.com>
2015-10-13 20:47:44 +0200

    init

---
+++ src/ImageServiceProvider.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ebess\ImageService;
+
+/**
+ * Class ImageServiceProvider
+ * @package Ebess\ImageService
+ */
+class ImageServiceProvider extends \Illuminate\Support\ServiceProvider
+{
+
+}
